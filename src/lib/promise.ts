0729dd1825b7aaadcd6bfde1a0faa6efc4484015
Dominic Bartl <dominic.bartl.at@gmail.com>
2020-06-09 14:59:42 +0200

    Add promiseProps() function

--- src/lib/promise.ts
+++ src/lib/promise.ts
@@ -81,3 +81,18 @@ export async function processPromisesParallelWithRetries<T, K>(
         throw error;
     }, onUpdate);
 }
+
+export async function promiseProps<T>(obj: {[K in keyof T]: Promise<T[K]> | T[K]}): Promise<T> {
+    const promises = [];
+    const keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        promises.push(obj[key as any]);
+    }
+    const results = await Promise.all(promises);
+
+    return results.reduce((map, current, index) => {
+        map[keys[index]] = current;
+        return map;
+    }, {});
+}
